96bed3c0219ae8bc9d624e2d7462c93c304aa8bc
Bandrabur Andrei <bandrabur.andrei@gmail.com>
2022-01-20 20:43:31 +0200

    implemented initial ManagerConturi.cpp

---
+++ ProiectITSchoolBank/ManagerConturi.cpp
@@ -0,0 +1,20 @@
+#include "ManagerConturi.h"
+
+std::string ManagerConturi::CreateIban()
+{
+	return std::string();
+}
+void ManagerConturi::adaugareCont()
+{
+	std::string nume, prenume, iban;
+	std::cout << "Introduceti numele persoanei: \n";
+	std::cin >> nume;
+	std::cout << "Introduceti prenumele persoanei: \n";
+	std::cin >> prenume;
+	//TODO: replace with createIban()
+	iban = "TestIban";
+	ContBancar* cont = new ContBancar(nume, prenume, iban);
+	m_listaConturi.push_back(cont);
+
+	system("cls");
+}
